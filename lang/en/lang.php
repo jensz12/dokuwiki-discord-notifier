c3d55c3b58b829d5eec75999becc614ad699856c
Jens Møller <jensz123@gmail.com>
2021-02-11 01:53:18 +0100

    The Midnight Wiki specific stuff

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl-2.0.html)
- * 
+ *
  * @author Rainbow-Spike <rainbow_spike@derpy.ru>
  */
 
@@ -11,9 +11,9 @@ $lang['t_updated'] = 'Newly updated';
 $lang['t_minor_upd'] = 'Newly minor updated';
 $lang['t_removed'] = 'Newly removed';
 
-$lang['e_created'] = 'created page';
-$lang['e_updated'] = 'updated page';
-$lang['e_minor_upd'] = 'minor updated page';
-$lang['e_removed'] = 'removed page';
+$lang['e_created'] = 'created the page';
+$lang['e_updated'] = 'updated the page';
+$lang['e_minor_upd'] = ' did a minor update to the page';
+$lang['e_removed'] = 'removed the page';
 
-$lang['compare'] = 'Compare changes';
+$lang['compare'] = 'Compare the changes';
